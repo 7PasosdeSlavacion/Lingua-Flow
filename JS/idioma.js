e5e4ec38948a87ab760bad07267fd1bd1b9d62b7
7PasosdeSlavacion <162037414+7PasosdeSlavacion@users.noreply.github.com>
2024-05-01 20:39:01 -0500

    Add files via upload

---
+++ JS/idioma.js
@@ -0,0 +1,47 @@
+/*const changeLanguage = async (language)=>{
+    const requestJson = await fetch(`./JSON/${language}.json`);
+    const texts = await requestJson.json();
+    
+    console.log(texts);
+    };
+    
+    function idioma(e) {
+      changeLanguage(e.target.parentElement.dataset.language);
+    };*/
+
+    const guardarIdiomaEnLocalStorage = (idioma) => {
+        localStorage.setItem('idioma', idioma);
+      };
+      
+      const cargarIdiomaDesdeLocalStorage = () => {
+        const idiomaGuardado = localStorage.getItem('idioma');
+        if (idiomaGuardado) {
+          changeLanguage(idiomaGuardado);
+        }
+      };
+      
+      const changeLanguage = async (language) => {
+        const requestJson = await fetch(`./JSON/${language}.json`);
+        const texts = await requestJson.json();
+      
+        for (const textChange of textsChange) {
+          const section = textChange.dataset.section
+          const value = textChange.dataset.value
+      
+          textChange.innerHTML = texts[section][value];
+        }
+      
+        guardarIdiomaEnLocalStorage(language);
+      };
+      
+      const flagsElement = document.getElementById("flags");
+      const textsChange = document.querySelectorAll("[data-section]");
+      
+      flagsElement.addEventListener("click", (e) => {
+        changeLanguage(e.target.parentElement.dataset.language);
+      });
+      
+      document.addEventListener('DOMContentLoaded', () => {
+        cargarIdiomaDesdeLocalStorage();
+      });
+      
